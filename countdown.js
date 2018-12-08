cffc73eeaa5b462fdb0da14861ade55f358e88f4
Pham Hieu <phamhieu1998@gmail.com>
2018-12-08 22:02:24 +0800

    refactor countdown.js

--- countdown.js
+++ countdown.js
@@ -1,14 +1,41 @@
-// Set the date we're counting down to
-var countDownDate = new Date().getTime() + 3*60*60*1000;
+function startCounter() {
+  auctionNetworkInstance.endTime((err, res) => {
+    if (err) {
+      console.error("getEndTime error: ", err);
+    } else {
+      console.log('getEndTime(): ', res);
+      var endTime = res.c[0];
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+      web3.eth.getBlockNumber((err, res) => {
+        if (err) {
+          console.error("web3.eth.blockNumber error: ", err);
+        } else {
+          console.log("web3.eth.blockNumber ", res);
+          var blockNumber = res;
+          web3.eth.getBlock(blockNumber, (err, res) => {
+            if (err) {
+              console.error("web3.eth.getBlock error: ", err);
+            } else {
+              // console.log("web3.eth.getBlock ", res);
+              var currentTime = res.timestamp;
+              console.log("current timestamp: ", currentTime);
+              var distance = endTime - currentTime;
+              console.log("distance = ", distance);
+              this.runCounter(distance);
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+function runCounter(distance) {
+  // Update the count down every 1 second
+  var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
     
     // Time calculations for days, hours, minutes and seconds
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -27,4 +54,5 @@ var x = setInterval(function() {
         document.getElementById("bid_dai_btn").disabled = true;
         document.getElementById("bid_knc_btn").disabled = true;
     }
-}, 1000);
+  }, 1000);
+}
