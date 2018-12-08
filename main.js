d16a585575fe20bfe8387a4aff465519898e3cf1
Pham Hieu <phamhieu1998@gmail.com>
2018-12-09 01:23:04 +0800

    refactor code

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ if (window.ethereum) {
 
 const kyberNetworkAddress = "0x91a502C678605fbCe581eae053319747482276b9";
 const auctionNetworkAddress = "0xcbf7B14076FE2C62D293E4c62F403b14B1fEa2eb";
-var account = null;
 var kyberNetworkInstance = null;
 var auctionNetworkInstance = null;
 var userEthAddress = null;
@@ -21,12 +20,6 @@ var highestBidValue = null;
 var userBidValue = null;
 var selected_coin = "eth";
 var artwork = null;
-
-setInterval(() => {
-  web3.eth.getAccounts().then((accounts) => {
-    account = accounts[0]
-  }).catch(console.error)
-}, 1000);
 
 function formatEthPrice(price) {
   const displayPrice = price / 1000000000000000000;
@@ -92,7 +85,7 @@ function doBiddingWithKnc(amount) {
 function doBidding(amount) {
   auctionNetworkInstance.methods.placeBid()
     .send({
-      from: account,
+      from: userEthAddress,
       value: Web3.utils.toWei(amount, 'ether')
     }).then((res) => {
     console.log('placeBid(): ', res);
@@ -156,18 +149,14 @@ try {
   console.log(auctionNetworkInstance);
 
   console.log(web3);
-  web3.eth.getAccounts((accountsErr, accounts) => {
-    if (accountsErr) {
-      console.error("Get user eth_address error: ",accountsErr)
-    } else {
-      console.log("getAccounts: ", accounts);
-      if (accounts && accounts.length > 0) {
-        userEthAddress = accounts[0];
-        this.updateFundsByBidder();
-        this.updateHighestBid();
-      }
+  web3.eth.getAccounts().then((accounts) => {
+    console.log("getAccounts: ", accounts);
+    if (accounts && accounts.length > 0) {
+      userEthAddress = accounts[0];
+      this.updateFundsByBidder();
+      this.updateHighestBid();
     }
-  });
+  }).catch(console.error)
 
   this.startCounter();
 } catch (err) {
