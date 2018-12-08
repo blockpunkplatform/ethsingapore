a5cfa9211dde986c26855197071dd7b64cb2d287
Pham Hieu <phamhieu1998@gmail.com>
2018-12-09 00:48:54 +0800

    clean code

--- main.js
+++ main.js
@@ -86,25 +86,6 @@ function startCounter() {
     var now = new Date().getTime() / 1000;
     var distance = endTime - now;
     this.runCounter(distance * 1000);
-
-    // web3.eth.getBlockNumber().then((res) => {
-    //     console.log("web3.eth.blockNumber ", res);
-    //     var blockNumber = res;
-    //     web3.eth.getBlock(blockNumber).then((res) => {
-    //       // console.log("web3.eth.getBlock ", res);
-    //       var currentTime = res.timestamp;
-    //       console.log("current timestamp: ", currentTime);
-    //       var distance = endTime - currentTime;
-    //       console.log("distance = ", distance);
-    //       this.runCounter(distance);
-    //     }).catch((err) => {
-    //       console.error("web3.eth.getBlock error: ", err);
-    //       return 0;
-    //     });
-    // }).catch((err) => {
-    //   console.error("getBlockNumber error: ", err);
-    //   return 0;
-    // });
   }).catch((err) => {
     console.error("endTime error: ", err);
     return 0;
